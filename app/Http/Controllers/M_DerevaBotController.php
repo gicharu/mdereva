788d69eb07db1dca0ab910498be087a2b935ca66
James Njoroge <patjayke@gmail.com>
2020-05-08 11:16:21 +0300

    first commit

---
+++ app/Http/Controllers/M_DerevaBotController.php
@@ -0,0 +1,123 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Questions;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Api;
+use Telegram\Bot\Exceptions\TelegramSDKException;
+use Telegram\Bot\Keyboard\Keyboard;
+use Telegram\Bot\Objects\Poll;
+use Telegram\Bot\Objects\PollOption;
+use Telegram\Bot\Objects\Update;
+
+class M_DerevaBotController extends Controller
+{
+
+    private $telegram;
+
+    public function __construct()
+    {
+        $this->telegram = new Api();
+    }
+
+    public function setWebHook()
+    {
+//        $url = secure_url("m_dereva-bot/" . env('TELEGRAM_BOT_TOKEN') . "/webhook");
+        $url = "https://bd200613.ngrok.io/" . env('TELEGRAM_BOT_TOKEN') . "/webhook";
+//        die($url);
+        try {
+            $this->telegram->setWebhook(['url' => $url]);
+        } catch (TelegramSDKException $e) {
+            Log::error($e->getMessage() . ' ' . $e->getFile() . ' ' . $e->getLine());
+        }
+
+    }
+
+    public function removeWebhook()
+    {
+        $this->telegram->removeWebhook();
+
+    }
+
+    public function webhook(Request $request)
+    {
+        $update = $this->telegram->getWebhookUpdate();
+        $message = $update->getMessage();
+        $chatId = null;
+        if ($update->isType('poll')) {
+            return $this->nextQuestion($update);
+        }
+        if (isset($message)) {
+
+            $username = $message->chat->firstName  . '_' . $message->chat->lastName;
+            Cache::put("username", $username);
+            Cache::put("$username.chat_id", $message->chat->id);
+            //$chatId = $update->getChat()->getId();
+            switch ($message->text) {
+                case "Begin free quiz":
+                    return $this->nextQuestion($update, $request);
+                    break;
+            }
+        }
+
+
+
+
+        return $this->start($update);
+
+    }
+
+    protected function start(Update $update)
+    {
+
+        $name = $update->getMessage()->getFrom()->getFirstName();
+//        echo $name; die;
+        $text = "Hello, $name! Please select an item from the menu to proceed";
+        $keyboard = Keyboard::make()
+            ->setResizeKeyboard(true)
+            ->row(Keyboard::button(['text' => "Begin free quiz"]))
+            ->row(Keyboard::button(['text' => "Register"]));
+//            ->row(Keyboard::button(['text' => "Learning stages"]))
+//            ->row(Keyboard::button(['text' => "Events"]))
+//            ->row(Keyboard::button(['text' => 'Contacts']));
+        return $this->telegram->sendMessage([
+            'chat_id' => $update->getChat()->getId(),
+            'text' => $text,
+            'reply_markup' => $keyboard
+        ]);
+    }
+
+    protected function nextQuestion(Update $update)
+    {
+        $question = Questions::inRandomOrder()->first();
+        $answers = $question->answers;
+
+        $quiz = new Poll($update);
+        $answersArray = [];
+        $answerOption = new PollOption($update);
+        $correctAnswer = 0;
+        foreach ($answers as $key  => $answer) {
+            $answersArray[$key] = $answerOption->text = $answer->answer;
+            if($answer->correct) {
+                $correctAnswer = $key;
+            }
+        }
+//        Log::debug($quiz->get('id')); die;
+        $quiz->options = $answersArray;
+        $username = Cache::get('username');
+        $chatId = Cache::get("$username.chat_id");
+        return $this->telegram->sendPoll([
+            'chat_id' => $chatId,
+            'type' => 'quiz',
+            'question' => $question->question,
+            'options' => $answersArray,
+            'correct_option_id' => $correctAnswer,
+        ]);
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Set Webhook
+|--------------------------------------------------------------------------
+| This route is used to set your webhook with Telegram,
+| just request from your browser once and then disable it.
+|
+| Example: http://domain.com/bot/set-webhook
+*/
+Route::get('/m-dereva-bot/set-webhook', 'M_DerevaBotController@setWebHook')->name('m_dereva-set-webhook');
+
+/*
+|--------------------------------------------------------------------------
+| Remove Webhook
+|--------------------------------------------------------------------------
+| This route is used to remove your webhook with Telegram,
+| just request from your browser once and then disable it.
+|
+| Example: http://domain.com/bot/remove-webhook
+*/
+Route::get('/m-dereva-bot/remove-webhook', 'M_DerevaBotController@removeWebhook')->name('m_dereva-remove-webhook');
+
+/*
+|--------------------------------------------------------------------------
+| Webhook (Incoming Handler)
+|--------------------------------------------------------------------------
+| This route handles incoming webhook updates.
+|
+| !! IMPORTANT !!
+| THIS IS AN INSECURE ENDPOINT FOR DEMO PURPOSES,
+| MAKE SURE TO SECURE THIS ENDPOINT, EXAMPLE: "/bot/<SECRET-KEY>/webhook"
+|
+| THEN SET THAT WEBHOOK WITH TELEGRAM.
+| SO YOU CAN BE SURE THE UPDATES ARE COMING FROM TELEGRAM ONLY.
+*/
+Route::post( '/' . env('TELEGRAM_BOT_TOKEN') . '/webhook', 'M_DerevaBotController@webhook')->name('m_dereva-webhook');
