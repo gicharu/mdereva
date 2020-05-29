19e23fc911c9e163de8682de4727277227258d1d
James Njoroge <patjayke@gmail.com>
2020-05-29 10:29:41 +0300

    fix chat id bug

--- app/Http/Controllers/M_DerevaBotController.php
+++ app/Http/Controllers/M_DerevaBotController.php
@@ -101,7 +101,7 @@ class M_DerevaBotController extends Controller
         $collectionCache = Cache::get("$username.collection");
         $collection = collect($collectionCache);
         $skipQuestions = [];
-        Log::debug("collection \n" . $collection);
+        Log::debug("question \n" . isset($quiz->question));
         if (isset($quiz->question)) {
             $skipQuestions = $collection->pluck('id');
             $answeredQuestion = $collection->pop();
